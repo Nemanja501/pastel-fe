4a47318fb24850420100a032efc0b15fa5dd478e
Nemanja501 <nemanjavulin5@gmail.com>
2024-11-28 15:34:24 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'signup', component: SignUpComponent},
+    {path: 'login', component: LoginComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
